f52fa571615aaebfd17f0fbc6945c7659a04e38e
Daryl L. L. Houston <daryl@automattic.com>
2018-01-14 18:30:37 -0500

    Update config to use heroku creds.

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 
+$db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 if(class_exists('DB')){
-	$db = new DB('localhost', '', '', '');
+	$db = new DB($db_url['host'], $db_url['user'], $db_url['pass'], substr($url["path"], 1 ) );
 	$db->connect();
 }
 
