4a92a4b70df1d4ded623fdc6c2d9dfb0de4502d6
Daryl L. L. Houston <daryl@automattic.com>
2018-01-16 22:13:26 -0500

    db fix

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php
 
 $db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-error_log( print_r( $db_url, true ) );
 
 if(class_exists('DB')){
-	$db = new DB($db_url['host'], $db_url['user'], $db_url['pass'], substr($url["path"], 1 ) );
+	$db = new DB($db_url['host'], $db_url['user'], $db_url['pass'], substr($db_url["path"], 1 ) );
 	$db->connect();
 }
 
