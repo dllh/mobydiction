901348fd9f23d5ae63d095b5b1e180cc09453826
Daryl L. L. Houston <daryl@automattic.com>
2018-01-16 22:12:36 -0500

    db debug

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
 $db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+error_log( print_r( $db_url, true ) );
 
 if(class_exists('DB')){
 	$db = new DB($db_url['host'], $db_url['user'], $db_url['pass'], substr($url["path"], 1 ) );
